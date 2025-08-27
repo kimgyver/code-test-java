ed756a28047990c1792a0e8d4c46cda40b9c5fb7
Jason <jason.kim@ambitai.com>
2025-08-27 19:35:29 +1200

    chagnes

--- src/TestTopKFrequent.java
+++ src/TestTopKFrequent.java
@@ -24,6 +24,19 @@ public class TestTopKFrequent {
         // 따라서, Java 16 이상을 사용하고 반환된 리스트를 수정할 필요가 없다면 .toList()를 사용하는 것이 더 간단할 수 있습니다.
         // stream() 메서드는 Java 8부터 도입되었으며, 컬렉션이나 배열에서 스트림을 생성하는 데 사용됩니다.
 
+        // 윗 부분 코드를 람다식 없이 작성하면 아래와 같습니다.
+//        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
+//        entryList.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+//            @Override
+//            public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2) {
+//                return e2.getValue().compareTo(e1.getValue()); // 내림차순 정렬
+//            }
+//        });
+//        List<Integer> result = new ArrayList<>();
+//        for (int i = 0; i < k && i < entryList.size(); i++) {
+//            result.add(entryList.get(i).getKey());
+//        }
+//        return result;
 
     }
 
